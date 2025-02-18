6a4530f394c38c0e896c96d3e44740e1dfc7cff2
lasalhettiarachchi <lasalshettiarachchi458@gmail.com>
2025-02-18 13:34:28 +0530

    [MS-ITP-001]: changes to gh actions

--- commands.sh
+++ commands.sh
@@ -1,4 +1,4 @@
-docker build . -t lasalhettiarachchi/postms:1.0.0
+docker build --platform=linux/amd64 . -t lasalhettiarachchi/postmsitp:1.0.0
 docker images
-docker run -p 8080:8080 lasalhettiarachchi/postms:1.0.0
-docker images push docker.io/lasalhettiarachchi/postms:1.0.0
+docker run -p 8080:8080 lasalhettiarachchi/postmsitp:1.0.0
+docker push docker.io/lasalhettiarachchi/postmsitp:1.0.0
